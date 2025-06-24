80161ba346448e44efd3573ef8cc7f88b0143415
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <pkdriller520@gmail.com>
2025-06-24 09:09:18 +0300

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -11,7 +11,7 @@ cmd({
     filename: __filename,
 },
 async (conn, mek, m, { from, reply }) => {
-    const githubRepoURL = 'https://github.com/Pkdriller/NEXUS-XMD';
+    const githubRepoURL = 'https://github.com/Pkdriller01/NEXUS-XMD';
 
     try {
         const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
@@ -172,7 +172,7 @@ ${repoData.description || 'No description provided'}
 
         // Send image with repo info
         await conn.sendMessage(from, {
-            image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/ug8w1x.jpg' },
+            image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/wbrnn4.jpg' },
             caption: selectedStyle,
             contextInfo: { 
                 mentionedJid: [m.sender],
